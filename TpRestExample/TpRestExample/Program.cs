798919ba64a105cb82663529b95f6bcaf5d0f5d8
Alex Fomin <Alex.Fomin@targetprocess.com>
2013-02-01 17:05:42 +0300

    Simple TP Rest example

---
+++ TpRestExample/TpRestExample/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace TpRestExample
+{
+	static class Program
+	{
+		private const string PathToTp = "http://localhost/TargetProcess/";
+
+		static void Main()
+		{
+			var client = new WebClient
+				{
+					Credentials = new NetworkCredential("admin", "admin"), // Use Basic Authentication
+					//Proxy = new WebProxy("fiddler-proxy",8888)
+				};
+
+			var userStoryCollection = DownloadUserStories(client);
+
+			foreach (var story in userStoryCollection.UserStories)
+			{
+				Console.WriteLine("{0} {1} {2}",story.Id, story.Name, story.Project);
+			}
+
+			var firstStory = userStoryCollection.UserStories.First();
+			firstStory.Name = "New name";
+
+			var result = UpdateUserStory(firstStory, client);
+			Console.WriteLine(result);
+		}
+
+		private static string UpdateUserStory(UserStory firstStory, WebClient client)
+		{
+			var ns = new XmlSerializerNamespaces();
+			ns.Add("", "");
+			var storySerializer = new XmlSerializer(typeof (UserStory));
+			var output = new StringWriter();
+
+			storySerializer.Serialize(output, firstStory, ns);
+			var serialized = output.ToString();
+
+			var result = client.UploadString(PathToTp + string.Format("api/v1/UserStories/{0}?include=[Id,Name,Project[Id,Name]]", firstStory.Id),
+			                                 "POST", serialized);
+			return result;
+		}
+
+		private static UserStoryCollection DownloadUserStories(WebClient client)
+		{
+			string xml = client.DownloadString(PathToTp + "api/v1/UserStories?include=[Id,Name,Project[Id,Name]]&where=Project.Id eq 2");
+
+			var serializer = new XmlSerializer(typeof (UserStoryCollection));
+			var userStoryCollection = (UserStoryCollection) serializer.Deserialize(new StringReader(xml));
+			return userStoryCollection;
+		}
+	}
+}
---
+++ TpRestExample/TpRestExample/UserStory.cs
@@ -0,0 +1,48 @@
+﻿using System.Xml.Serialization;
+
+namespace TpRestExample
+{
+	[XmlRootAttribute(Namespace = "", IsNullable = false, ElementName = "UserStories")]
+	public class UserStoryCollection
+	{
+		public UserStoryCollection()
+		{
+			UserStories = new UserStory[0];
+		}
+
+		[XmlElement("UserStory")]
+		public UserStory[] UserStories { get; set; }
+
+		[XmlAttribute]
+		public string Next { get; set; }
+	}
+
+	[XmlRootAttribute(Namespace = "", IsNullable = false, ElementName = "UserStory")]
+	public class UserStory
+	{
+		[XmlAttribute]
+		public int Id { get; set; }
+
+		[XmlAttribute]
+		public string Name { get; set; }
+
+		public Project Project { get; set; }
+	}
+
+	[XmlRootAttribute(Namespace = "", IsNullable = false, ElementName = "Project")]
+	public class Project
+	{
+		[XmlAttribute]
+		public int Id { get; set; }
+
+		[XmlAttribute]
+		public string Name { get; set; }
+
+		public override string ToString()
+		{
+			return string.Format("Id: {0}, Name: {1}", Id, Name);
+		}
+	}
+
+
+}
