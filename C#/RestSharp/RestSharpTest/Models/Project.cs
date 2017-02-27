1c2375348c616e0732ae221aa3771514a5837a04
Julia Pastushenko <julia@targetprocess.com>
2017-02-28 00:03:40 +0300

    Sample for creating Request with attachment

---
+++ C#/RestSharp/RestSharpTest/Models/Project.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace RestSharpTest.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ C#/RestSharp/RestSharpTest/Models/Request.cs
@@ -0,0 +1,10 @@
+﻿namespace RestSharpTest.Models
+{
+    public class Request
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Project Project { get; set; }
+    }
+}
